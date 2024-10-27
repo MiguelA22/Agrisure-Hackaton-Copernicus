e608498d50cdd2e90659c29bba3939825a21ba34
Miguel Pineda <106281383+MiguelA22@users.noreply.github.com>
2024-10-27 18:22:31 -0500

    Update llmEngine.py

--- AgriSure/llmEngine.py
+++ AgriSure/llmEngine.py
@@ -5,18 +5,18 @@ from dotenv import load_dotenv, find_dotenv
 from formateo import formato_resp_llm
 from langchain_together import Together
 
-# Load environment variables
+# Cargar variables del token
 load_dotenv(find_dotenv())
 TOGETHER_API_KEY = os.getenv("TOGETHER_API_KEY")
 
-# Configure the Anthropic LLM
+# Conffigurar modelo de Together
 llm = Together(
-    model="meta-llama/Llama-3-70b-chat-hf",  # Free model
-    together_api_key = TOGETHER_API_KEY,  # Explicitly passing API key
-    temperature=0.7  # Added temperature for controlled randomness
+    model="meta-llama/Llama-3-70b-chat-hf",  
+    together_api_key = TOGETHER_API_KEY, 
+    temperature=0.7  
 )
 
-# Create template for the agricultural report prompt
+# Crear template para el LLM
 template = """
 Genera un informe sobre el rendimiento agrícola basado en las siguientes métricas y que se cultiva {rubro}:
 - NDVI (Índice de vegetación): {ndvi_mean}
@@ -28,22 +28,22 @@ Genera un informe sobre el rendimiento agrícola basado en las siguientes métri
 Indica el estado general del terreno, la salud de los cultivos y recomendaciones para mejorar.
 """
 
-# Create the LangChain prompt
+# Crear el prompt
 prompt = PromptTemplate(
     input_variables=["ndvi_mean", "evi_mean", "ndmi_mean", "ndwi_mean", "barren_mean", "rubro"],
     template=template
 )
 
-# Create the LangChain chain
+# Crear chain
 llm_chain = LLMChain(llm=llm, prompt=prompt)
 
-# Generate the report based on the data
-def generar_informe_agri(data, rubro):  # Fixed typo in function name
+# Generar el reporte de agricultura
+def generar_informe_agri(data, rubro):  
     try:
-        # Format the data
+        # Obtener coordenadas formateadas del poligono 
         ndvi_mean, evi_mean, ndmi_mean, ndwi_mean, barren_mean = formato_resp_llm(data)
         
-        # Generate the report
+        # Generar reporte
         informe = llm_chain.run({
             "ndvi_mean": ndvi_mean,
             "evi_mean": evi_mean,
